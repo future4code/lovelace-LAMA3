c6e82e8d0155814131c0882ecec711da8a222b11
erlan carvalho <erlangleidson@hotmail.com>
2021-11-12 19:39:44 -0300

    Criei o endpoint de signup (cadastro de usuarios).

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,9 @@
 import express, {Express} from "express";
 import cors from "cors";
-import {AddressInfo} from "net";
 
 export const app:Express = express();
 
 app.use(express.json());
 app.use(cors());
 
-export const server = app.listen(process.env.PORT || 3003, ()=>{
-    const address = server.address() as AddressInfo;
 
-    if(server){
-        console.log(`Server is runing in http://localhost:${address.port}`)
-    }else{
-        console.log(`Error upon stating the Server`)
-    }
-})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
 import {app} from "../src/app";
+import { signUpController } from "./controller/endpoint/signUpController";
+import {AddressInfo} from "net";
+
+
+//###############
+//## Endpoints ##
+//###############
+
+app.post("/user/signup", signUpController)
+
+//###############
+//## Endpoints ##
+//###############
+
+
+
+export const server = app.listen(process.env.PORT || 3003, ()=>{
+    const address = server.address() as AddressInfo;
+
+    if(server){
+        console.log(`Server is runing in http://localhost:${address.port}`)
+    }else{
+        console.log(`Error upon starting the Server`)
+    }
+})
